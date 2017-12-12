cea456fcba8afd04e24683a26db9a50fd8997253
wmq <1214520313@qq.com>
2017-12-12 17:27:03 +0800

    这是我最终提交的项目一

---
+++ item1/js/showsearch.js
@@ -0,0 +1,63 @@
+var txt = document.getElementById("txt");
+var searchBtn = document.getElementById('search');
+var content = document.getElementById("content");
+var oUl = document.querySelector(".header>ul");
+var url = location.href;
+var arr = url.split('?');
+var preValue = decodeURIComponent(arr[1]) ;
+
+searchBtn.onmouseover = function(){
+	this.style.cursor = "pointer";
+}
+ajax("post","json/cake-data.json","",function(obj){
+	txt.oninput = function(){
+		oUl.style.display = "block";
+		var str1 = '';
+		var count = 0;
+		for(var i=0;i<obj.length;i++){
+			console.log(txt.value);
+			if(obj[i].title.indexOf(txt.value)!=-1){
+				count++;
+				if(count<=5){
+					str1 +='<li data-id='+obj[i].id+'>'+obj[i].title+'</li>';
+				}
+			}
+		}
+		oUl.innerHTML = str1;
+	}
+	
+	var str = '';
+	console.log(preValue);
+	for(var i=0;i<obj.length;i++){
+		if(obj[i].title.indexOf(preValue)!=-1){	
+			str+='<li data-id='+obj[i].id+'><p><img src='+obj[i].images+' /></p><p><i>店取</i><a href="##">'+obj[i].title+'</a></p><p>'+obj[i].price+'<em>'+obj[i].price+'</em></p></li>'
+		}
+	}
+	content.innerHTML = str;
+	searchBtn.onclick = function(){
+		var str = '';
+		for(var i=0;i<obj.length;i++){
+			if(obj[i].title.indexOf(txt.value)!=-1){	
+				str+='<li data-id='+obj[i].id+'><p><img src='+obj[i].images+' /></p><p><i>店取</i><a href="##">'+obj[i].title+'</a></p><p>'+obj[i].price+'<em>'+obj[i].price+'</em></p></li>'
+			}
+		}
+		content.innerHTML = str;
+	}
+})
+ oUl.onclick = function(e){
+ 	var e = e|| event;
+ 	var target = e.target || e.srcElement;
+ 	if(target.tagName == "LI"){
+ 		var id = target.getAttribute("data-id");
+ 		location.href="detail.html?"+id;
+ 	}
+ }
+ content.onclick = function(e){
+		var e = e||event;
+		var target = e.target||e.srcElement;
+		if(target.tagName=='A'){
+			var id = target.parentNode.parentNode.getAttribute('data-id');
+			console.log(id);
+			location.href='detail.html?'+id;
+		}
+	}
